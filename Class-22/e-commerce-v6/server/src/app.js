790c4080ae070facbb732c2224dc29a33ef40064
Sabeel Khan <sabeelkhan@Sabeels-MacBook-Air.local>
2024-10-12 21:21:00 +0530

    added mongoose to products

--- Class-22/e-commerce-v6/server/src/app.js
+++ Class-22/e-commerce-v6/server/src/app.js
@@ -2,20 +2,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-// Routes 
+// Import Routes 
 const productRoutes = require('./routes/product');
 const userRoutes = require('./routes/user');
 const { NotFoundError } = require('./core/ApiError');
 
-app.use(cors({
-    origin: ['http://localhost:5173'],
-    credentials: true
-}));
+// Pre-Defined Middlewares
+app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(express.json({limit:'10mb'}));
 
-app.use("/api/v1", productRoutes);
+// APIs
+app.use("/api/v1/products", productRoutes);
 app.use('/api/v1/users', userRoutes);
-
 
 app.all("*", (req, res, next) => next(new NotFoundError()));
 
